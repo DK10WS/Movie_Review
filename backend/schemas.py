fbe37e0fff9e6bcf24b5174850e5cf27b95d01a9
Dhruv Kunzru <dhruvkunzru@Gmail.com>
2025-06-10 13:33:23 +0530

    Function: Login

--- backend/schemas.py
+++ backend/schemas.py
@@ -1,7 +1,22 @@
 from pydantic import BaseModel, EmailStr
+from email.policy import strict
 
 class UserCreate(BaseModel):
     username: str
     fullname: str
     email: EmailStr
     password: str
+
+class LoginCheck(BaseModel):
+    email: EmailStr
+    password: str
+
+class ReadUser(BaseModel):
+    username: str
+    fullname: str
+    email: str
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
--- backend/userAUTH/auth.py
+++ backend/userAUTH/auth.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, HTTPException, Depends
 from sqlalchemy.orm import Session
 from passlib.context import CryptContext
 from userAUTH.connection import get_db
-from schemas import UserCreate
+from schemas import UserCreate , LoginCheck
 from userAUTH.Model import User
 
 router = APIRouter()
@@ -10,6 +10,9 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def hash_password(password: str) -> str:
     return pwd_context.hash(password)
+
+def verify_password(normal_password, hashed_password):
+    return pwd_context.verify(normal_password, hashed_password)
 
 @router.post("/register/")
 
@@ -30,4 +33,16 @@ def register_user(creds: UserCreate, db: Session = Depends(get_db)):
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
-    return new_user
+    return {"Message" : new_user.fullname + "You are Registered"}
+
+@router.post("/login")
+def login(cred: LoginCheck, db: Session = Depends(get_db)):
+    user = db.query(User).filter(User.email == cred.email).first()
+
+    if not user:
+        raise HTTPException(status_code=400, detail="User does not exist")
+
+    if not verify_password(cred.password, user.password):
+        raise HTTPException(status_code=400, detail="Password is incorrect")
+
+    return {"message": "Logged in GG"}
