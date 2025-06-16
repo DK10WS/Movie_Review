0a37c441a6f9919e1dd8b9d42c00753b0d176a89
Dhruv Kunzru <dhruvkunzru@Gmail.com>
2025-06-16 11:40:27 +0530

    Update: depricated lifespan functions

--- backend/main.py
+++ backend/main.py
@@ -1,15 +1,29 @@
+from contextlib import asynccontextmanager
+
 from connection import Connect, engine
-from fastapi import FastAPI, HTTPException, Request, status
+from fastapi import FastAPI, Request
 from middleware.middleware import JWTAuthMiddleware
-from movies.movies import routers
+from movies.movies import routers as movie_routers
 from userAUTH.auth import router
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # connect
+    await Connect()
+    yield
+    # disconnect
+    await engine.dispose()
+    print("Connection is closed")
+
+
+app = FastAPI(lifespan=lifespan)
 app.add_middleware(JWTAuthMiddleware)
 
 app.include_router(router)
-app.include_router(routers)
-
+app.include_router(movie_routers, prefix="/movies", tags=["Movies"])
+""""
+Depricated
 
 @app.on_event("startup")
 def startup():
@@ -20,6 +34,8 @@ def startup():
 def shutdown_event():
     engine.dispose()
     print("Connection is closed")
+
+"""
 
 
 @app.get("/")
