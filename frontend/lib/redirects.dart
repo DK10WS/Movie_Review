ef6f09d64d88661fed7eafb559f38ec179e0a85f
Dhruv Kunzru <dhruvkunzru@Gmail.com>
2025-06-29 00:55:26 +0530

    Test: Build test with new url

--- frontend/lib/redirects.dart
+++ frontend/lib/redirects.dart
@@ -1,6 +1,6 @@
 // Auth
 
-String baseurl = "https://dkslist.dk10.tech/api";
+String baseurl = "https://dk10list.dk10.tech/api";
 String login = "$baseurl/login";
 String Whoami = "$baseurl/whoami";
 String sendotp = "$baseurl/sendotp";
